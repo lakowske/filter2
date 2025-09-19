2282dde869760b4901f198b4c24f554aec669db6
Seth Lakowske <lakowske@gmail.com>
2025-09-19 16:44:29 -0500

    Initial project setup for filter

---
+++ src/filter/__init__.py
@@ -0,0 +1,10 @@
+"""A kanban project management tool."""
+
+__version__ = "0.1.0"
+__author__ = "Seth Lakowske"
+__email__ = "lakowske@gmail.com"
+
+from .actions.build import build
+from .core import calculate_sum, greet
+
+__all__ = ["greet", "calculate_sum", "build"]
